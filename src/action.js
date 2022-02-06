77ecbe33a53df164a3cd85287f6a6b2a394456c3
Nabil Hassan <nabil.hassan@clearchannel.co.uk>
2022-02-06 22:55:13 +0000

    ...

--- src/action.js
+++ src/action.js
@@ -1,5 +1,19 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 async function run() {
   console.log('Hello, world!');
+
+  const TOKEN = core.getInput('TOKEN');
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  await octokit.issues.createComment({
+      ...context.repo,
+      issue_number: pull_request.number,
+      body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
+  });  
+
 }
 
 run();
